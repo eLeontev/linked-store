e0a65ddc6998c2b5ea36e13df193d6435da48f99
eLeontev <joonior199@gmail.com>
2020-07-27 17:07:40 +0300

    prepared API to publish

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { simpleStore, derivedStore } from './units/units';
+export {
+    useLinkedState,
+    useLinkedStateValue,
+    useResetLinkedState,
+    useSetLinkedState,
+} from './hooks';
