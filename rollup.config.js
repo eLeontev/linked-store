4b0d7ce6b0c29eaa7947f6c0dc5af81e3ec8237c
eLeontev <joonior199@gmail.com>
2020-07-31 11:31:16 +0300

    ✨ #2 add production build for package with production postfix. 💄 recombine scripts related to build package

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,26 @@
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
-export default {
-    plugins: [typescript()],
-    input: 'src/index.ts',
-    output: {
-        file: `dist/index.js`,
-        format: 'es',
-        name: 'LinkedStore',
-        exports: 'named',
+export default [
+    {
+        plugins: [typescript()],
+        input: 'src/index.ts',
+        output: {
+            file: `dist/index.js`,
+            format: 'es',
+            name: 'LinkedStore',
+            exports: 'named',
+        },
     },
-};
+    {
+        plugins: [typescript()],
+        input: 'src/index.ts',
+        output: {
+            file: `dist/index.production.js`,
+            format: 'es',
+            name: 'LinkedStore',
+            exports: 'named',
+            plugins: [terser()],
+        },
+    },
+];
